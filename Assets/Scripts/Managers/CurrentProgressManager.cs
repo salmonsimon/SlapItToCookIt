a82150a20a2486fff7ed9a511de30199103df30f
salmonsimon <38538239+salmonsimon@users.noreply.github.com>
2023-01-04 15:12:35 -0600

    Slap and Temperature Mechanism (#4)

--- Assets/Scripts/Managers/CurrentProgressManager.cs
+++ Assets/Scripts/Managers/CurrentProgressManager.cs
@@ -8,15 +8,18 @@ public class CurrentProgressManager : MonoBehaviour
     [SerializeField] private float timePlayed = 0f;
     [SerializeField] private int slapCount = 0;
 
+    private bool finishedSlapping = false;
+    public bool FinishedSlapping { get { return finishedSlapping; } set { finishedSlapping = value; } }
+
     private void Update()
     {
-        if (!GameManager.instance.IsOnMainMenu())
+        if (!GameManager.instance.IsOnMainMenu() && !FinishedSlapping)
             timePlayed += Time.deltaTime;
     }
 
-    public void IncreaseSlapCount()
+    public void IncreaseSlapCount(int value)
     {
-        slapCount++;
+        slapCount += value;
         GameManager.instance.GetCountersUI().UpdateCounters();
     }
 
