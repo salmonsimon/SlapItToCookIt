a008f4a7814ae825ef4d4dfdc294388624803588
salmonsimon <simon.bahamonde@gmail.com>
2023-01-08 22:16:16 -0300

    added player default progress initializator and home keeping

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,5 +1,9 @@
+using System.Linq;
+
 public static class Utils
 {
+    private static System.Random random = new System.Random();
+
     public static string FloatToTimeFormat(float timeInput)
     {
         double timePlayedDouble = (double)timeInput;
@@ -9,5 +13,13 @@ public static class Utils
         string displayTime = time.ToString("hh':'mm':'ss");
 
         return displayTime;
+    }
+
+
+    public static string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
     }
 }
