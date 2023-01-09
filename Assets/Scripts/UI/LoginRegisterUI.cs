027ca0bd772eecd2627a28890cf3f8cc1946d903
salmonsimon <38538239+salmonsimon@users.noreply.github.com>
2023-01-08 22:20:04 -0300

    added player default progress initializator and home keeping (#9)

--- Assets/Scripts/UI/LoginRegisterUI.cs
+++ Assets/Scripts/UI/LoginRegisterUI.cs
@@ -6,6 +6,7 @@ using PlayFab;
 using PlayFab.ClientModels;
 using Newtonsoft.Json;
 using System.Xml.Serialization;
+using PlayFab.PfEditor.Json;
 
 public class LoginRegisterUI : MonoBehaviour
 {
@@ -24,6 +25,7 @@ public class LoginRegisterUI : MonoBehaviour
 
     public void RegisterNewUserButton()
     {
+        ClearMessageText();
         RegisterNewUser(registerEmailInput, registerPasswordInput);
     }
 
@@ -31,7 +33,7 @@ public class LoginRegisterUI : MonoBehaviour
     {
         if (password.text.Length < 6)
         {
-            messageText.text = "Password must contain at least 6 characters";
+            StartCoroutine(ShowMessage("Password must contain at least 6 characters"));
         }
 
         var request = new RegisterPlayFabUserRequest
@@ -46,7 +48,7 @@ public class LoginRegisterUI : MonoBehaviour
 
     private void OnRegisterSuccess(RegisterPlayFabUserResult result)
     {
-        messageText.text = "Registered, logging in...";
+        StartCoroutine(ShowMessage("Registered, logging in..."));
 
         ToMainMenu();
     }
@@ -55,15 +57,28 @@ public class LoginRegisterUI : MonoBehaviour
 
     #region Login
 
+    #region Buttons
     public void LoginWithEmailButton()
     {
+        ClearMessageText();
         LoginWithEmail(loginEmailInput, loginPasswordInput);
     }
 
     public void LoginWithDeviceButton()
     {
+        ClearMessageText();
         LoginWithDevice();
     }
+
+    public void ResetPasswordButton()
+    {
+        ClearMessageText();
+        CheckIfAccountExists(loginEmailInput);
+    }
+
+    #endregion
+
+    #region Login
 
     public void LoginWithDevice()
     {
@@ -89,16 +104,65 @@ public class LoginRegisterUI : MonoBehaviour
 
     private void OnLoginSuccess(LoginResult result)
     {
-        messageText.text = "logging in...";
+        StartCoroutine(ShowMessage("logging in..."));
 
-        ToMainMenu();
+        InitializePlayerProgressIfNeeded();
     }
 
-    public void ResetPasswordButton()
+    private void InitializePlayerProgressIfNeeded()
     {
-        ResetPassword(loginEmailInput);
+        var initializePlayerRequest = new ExecuteCloudScriptRequest()
+        {
+            FunctionName = "InitializePlayerIfNeeded",
+            GeneratePlayStreamEvent = true
+        };
+
+        PlayFabClientAPI.ExecuteCloudScript(initializePlayerRequest, OnInitializationResponse, OnError);
     }
 
+    private void OnInitializationResponse(ExecuteCloudScriptResult result)
+    {
+        var lastLog = result.Logs[result.Logs.Count - 1];
+
+        if (lastLog.Level == "Error") 
+        {
+            StartCoroutine(ShowMessage(lastLog.Message));
+        }
+        else
+        {
+            ToMainMenu();
+        }
+    }
+
+    #endregion
+
+    #region Password Reset
+
+    private void CheckIfAccountExists(InputField email)
+    {
+        var mockPassword = Utils.RandomString(8);
+
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = email.text,
+            Password = mockPassword
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress(request, null, OnAccountExistsCheck);
+    }
+
+    private void OnAccountExistsCheck(PlayFabError error)
+    {
+        if (error.ErrorMessage == "Invalid email address or password")
+        {
+            ResetPassword(loginEmailInput);
+        }
+        else
+        {
+            StartCoroutine(ShowMessage(error.ErrorMessage));
+        }
+    }
+ 
     private void ResetPassword(InputField email)
     {
         var request = new SendAccountRecoveryEmailRequest
@@ -112,8 +176,10 @@ public class LoginRegisterUI : MonoBehaviour
 
     private void OnPasswordReset(SendAccountRecoveryEmailResult result)
     {
-        messageText.text = "Password reset email sent";
+        StartCoroutine(ShowMessage("Password reset email sent"));
     }
+
+    #endregion
 
     #endregion
 
@@ -121,7 +187,7 @@ public class LoginRegisterUI : MonoBehaviour
 
     private void OnError(PlayFabError error)
     {
-        messageText.text = error.ErrorMessage;
+        StartCoroutine(ShowMessage(error.ErrorMessage));
     }
 
     private void ToMainMenu()
@@ -132,6 +198,15 @@ public class LoginRegisterUI : MonoBehaviour
     public void ClearMessageText()
     {
         messageText.text = string.Empty;
+    }
+
+    private IEnumerator ShowMessage(string message)
+    {
+        messageText.text = message;
+
+        yield return new WaitForSeconds(3f);
+
+        ClearMessageText();
     }
 
     #endregion
